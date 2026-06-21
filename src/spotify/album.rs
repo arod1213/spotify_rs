620943a0ed3705d1082633fafa08112acdba1469
arod1213 <aidan@aidanrodriguez.com>
2026-06-21 15:56:39 -0700

    add album images

--- src/spotify/album.rs
+++ src/spotify/album.rs
@@ -10,6 +10,11 @@ use crate::spotify::{
 };
 
 #[derive(Debug, Deserialize, Serialize)]
+pub struct AlbumImage {
+    pub url: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct AlbumMeta {
     pub upc: String,
 }
@@ -19,6 +24,9 @@ pub struct Album {
     pub id: String,
     pub name: String,
     pub label: String,
+
+    #[serde(default)]
+    pub images: Vec<AlbumImage>,
 
     #[serde(deserialize_with = "deserialize_date")]
     pub release_date: NaiveDate,
